48aeba6b72e705b926e330a35d20b0c786b15168
elvis210 <hoelvis@gmail.com>
2025-07-19 02:51:50 -0700

    feat: deals, login

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://csiigcklpcekcvgaukpt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzaWlnY2tscGNla2N2Z2F1a3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MzIzMDYsImV4cCI6MjA2ODEwODMwNn0.mGL8SjuEfDURlO9zpLYb3M6rUA_NSQ6FAC8sawxEKBg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
